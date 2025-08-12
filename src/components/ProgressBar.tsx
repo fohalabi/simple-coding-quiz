bd529082793327dd30d5d9a235e062bc66255b24
fohalabi <ayomideabimbola79@gmail.com>
2025-08-12 02:34:48 +0100

    added Progressbar props

---
+++ src/components/ProgressBar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ProgressBarProps } from './types';
+
+const ProgressBar: React.FC<ProgressBarProps> = ({ current, total}) => (
+    <div className='w-full bg-gray-200 rounded-full h-2 mb-4'>
+        <div
+            className='br-blue-600 h-2 rounded-full transition-all duration-300'
+            style={{ width: `${((current + 1 / total) * 100)}`}}
+        ></div>
+    </div>
+);
+
+export default ProgressBar;
